d8066fb09cc0e20b3f5aa6d38622c44e0577757f
Trataku <dylanmo13@yahoo.com>
2017-12-04 22:23:36 -0600

    Page 1 Polish
    Polished Page 1 to look more presentable

--- index.php
+++ index.php
@@ -36,13 +36,19 @@
                             <input type="text" id="user" name="user" class="ui-widget-content ui-corner-all" autofocus value="<?php print $user; ?>" placeholder="Password">
                             <input type="password" id="pass" name="pass" class="ui-widget-content ui-corner-all">
                         </fieldset>
-                        <fieldset id="submitButton">
-                            <input type="submit" value="Submit">
+                        <fieldset>
+                            <input type="submit" value="Submit" id="submitButton">
                         </fieldset>
                     </form>
                 </div>
             </li>
         </ul> 
+    </div>
+    <div id="wrapper">
+        <div id="aboutBox">
+            <p id="aboutBoxPara">Welcome to the Pokemon Generation 1 Information Widget! We aim to give information about all Generation 1 Pokemon in an easy to use and friendly way. Take a look at pokemon information, or login to build your own team and compare their strengths!</p>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/rg6CiPI6h2g" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+        </div>
     </div>
 </body>
 </html>
